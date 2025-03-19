9aad25aaf8dbc77f305534d63558b7549e6041f6
NoobikNovi4ok <antonpetya1330@mail.ru>
2025-03-19 19:44:30 +0300

    Some_templates_change

--- clireq/templatetags/custom_filters.py
+++ clireq/templatetags/custom_filters.py
@@ -4,7 +4,10 @@ register = template.Library()
 
 
 @register.filter
-def format_phone(phone):
+def format_phone(phone) -> str:
+    """
+    Форматирует телефонный номер в виде +7(XXX)-XXX-XX-XX
+    """
     if phone and len(phone) == 11:
         return f"+{phone[:1]}({phone[1:4]})-{phone[4:7]}-{phone[7:9]}-{phone[9:]}"
     return "Некорректный номер телефона"
